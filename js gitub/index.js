eb415041893c1090e246ae93526d041803c6d012
suthermukes827 <suthermukesh827>
2021-03-03 21:31:28 +0500

    work on js

---
+++ js gitub/index.js
@@ -0,0 +1,203 @@
+function functionlike() {
+    alert("liked")
+    
+}
+function functioncomment() {
+    alert(" Thanks for comment")
+    
+}
+function functionshare() {
+    alert("share")
+    
+}
+function functionsubscribe() {
+    alert("subsribe")
+    
+}
+function functionFallow() {
+    alert("Fallow")
+    
+}
+
+alert("Thanks........!")
+
+// var for string
+
+var name="mukesh";
+
+var firstname="Mukesh kumar";
+
+var lastname="suther"
+
+var name="Thanks for input";
+alert("thanks")
+
+
+// var for number
+
+var size="15";
+
+var size="30";
+var width="20";
+
+var num=50;
+num=num +100
+
+
+
+var orignalnum="24";
+var newnum= orignalnum +10;
+
+
+var num="23";
+alert("num");
+
+
+// Variable Names Legal and Illegal
+
+// Illegal
+var x="name"
+
+// Legal
+var username="Mukesh";
+// better than x
+
+
+// Math expressions Familiar operators
+
+var importantNumber = 4;
+
+var number= 4 + 6
+alert("4+6")
+
+var num = 3-2
+alert("3-2")
+
+
+var num = 10;
+var othernum= 1;
+var allnumber= num + othernum;
+
+// also we use multiple operation of mathamatics eg= +, -, *, /, 
+// and we use % eg=
+var persantage= 9 % 2;
+
+
+
+// Math expressions Unfamiliar operators
+
+num++;
+
+num = num + 4
+
+
+var num = 10;
+var newNum = num++
+
+
+var num = 1;
+var newNum = num--;
+
+
+// Math expressions Eliminating ambiguity
+
+var cost = 3 + (4+3);
+
+var cost =( 3+2 ) + 2;
+
+ number =(( 2 * 3) *3) + 6;
+
+ multy= ( 2 + 4) *( 5 + 2);
+
+
+
+ var letter = "vfdfdgffdgga"
+     alert("a letter")
+ 
+
+var comment="thanks for comment"
+
+var like ="thanks for like"
+
+var share ="Thanks for share"
+ alert("comment + like + share");
+ 
+//  we use number in string also in alert 
+// example=
+alert("2" + "4")
+
+// we push and dispaly reslt is (22)
+
+// prompt
+
+// we get detail of user
+
+var name=prompt("what is your name....??")
+
+
+// if statements
+var fathername=prompt("bhuralal");{
+    alert("correct")
+}
+
+if (name ===mukesh);
+    if(fathername ===bhurarlal);
+    if( cast ===suthr);
+        else{
+            alert("You are selected")
+
+        }
+
+        // Comparison operators
+
+        if (yourname === "mukesh" + " " + "bhuralal" );{
+            alert("you are slected")
+        }
+
+        if( yourmobilenumber !=="7564546566");{
+            alert("you can not conttact him....!")
+        } 
+
+        if ( 4 > 3);{
+            alert("good")
+        }
+        if ( 1 < 0 );{
+            alert("good")
+        }
+
+        if ( 1 >= 0 ){
+            alert("ok")
+        }
+
+        if ( 2 <= 4);{
+            alert("done")
+        }
+
+        var name=prompt("what is your name");
+        if (x === "Mukesh" );{
+           alert("correct")
+        }
+
+
+        if (y !== "mukesh");{
+            alert(" wrong answer")
+        }
+
+        if (weight > 30 && time > 6) {
+
+            alert("done you are selected");
+        }
+
+        
+
+
+
+
+
+
+
+
+ 
+
+
+
